96b2ad15995c46b2011e48c1f22b4b7e1a5d58e1
campion-luca <campionluca3@gmail.com>
2024-12-04 11:40:58 +0100

    NavBar update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import './App.css';
-import NavBar from './components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css' // import di bootstrap
-import Footer from './components/Footer';
-import Title from './components/Title';
-import Body from './components/Body';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import NavBar from './components/NavBar';
+import { Body } from './components/Body';
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <NavBar />
-      </header>
-      <body>
-        <Title />
-        <Body />
-      </body>
-      <footer>
-        <Footer />
-      </footer>
+    <div className="bg-color-rolex">
+    <BrowserRouter>
+    
+    <NavBar />
+
+        <Routes>
+        {/* not found */}
+        <Route path="/" element={<Body />} />
+        </Routes>
+
+    </BrowserRouter>
+
     </div>
   );
 }
 
-export default App;
+export default App
---
+++ src/components/LoginPage.jsx
@@ -0,0 +1,16 @@
+import { Component } from "react";
+import NavBar from "./NavBar";
+// import { Login } from "./Login";
+
+class LoginPage extends Component {
+    render () {
+        return (
+            <>
+            <NavBar />
+            {/* <Login /> */}
+            </>
+        )
+    }
+}
+
+export default LoginPage
